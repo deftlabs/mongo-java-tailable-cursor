ecdb92bd59c99db1c6eacde0e45d3739335fb0ee
Ryan <rgnitz@gmail.com>
2011-11-22 16:50:40 -0500

    initial load

---
+++ README.md
@@ -0,0 +1,34 @@
+
+mongo-java-distributed-lock
+=============
+
+About
+------------
+
+An extension to the [MongoDB](http://www.mongodb.org/) Java driver that supports a blocking [tailable cursor](http://www.mongodb.org/display/DOCS/Tailable+Cursors).
+
+This library is currently in alpha.
+
+Usage
+------------
+
+* [Javadocs](http://api.deftlabs.com/mongo-java-tailable-cursor)
+* [Getting Started](https://github.com/deftlabs/mongo-java-tailable-cursor/wiki/Getting-Started)
+
+License
+------------
+
+Copyright 2011, Deft Labs.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ src/test/com/deftlabs/cursor/mongo/TailableCursorIntTests.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2011, Deft Labs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deftlabs.cursor.mongo;
+
+// Mongo
+import com.mongodb.Mongo;
+import com.mongodb.MongoURI;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+
+// JUnit
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+// Java
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Test the distributed lock. You must be running mongo on localhost:27017 for this
+ * test to work.
+ */
+public final class TailableCursorIntTests {
+
+    @Test
+    public void testPlaceholder() throws Exception {
+
+    }
+
+    @Before
+    public void init() throws Exception {
+
+    }
+
+    @After
+    public void cleanup() {
+
+    }
+
+
+}
+
